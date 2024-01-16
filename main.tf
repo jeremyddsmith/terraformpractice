2f2499dcda3729bb11d12653c6e4078b9b6a4a5a
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-16 13:47:43 -0500

    Thank you for the commit

--- main.tf
+++ main.tf
@@ -17,37 +17,37 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  assign_generated_ipv6_cidr_block = true 
-  enable_dns_hostnames = true
-  enable_dns_support = true 
+  cidr_block                       = var.vpc_cidr
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
   tags = {
     "Name" = "${var.default_tags.env}-VPC"
-    
+
   }
 }
 #Public Subnet: 10.0.0.0/24 ensuring no subnets have the same address
 resource "aws_subnet" "public" {
-    count = var.public_subnet_count
-    vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-    ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
-    map_public_ip_on_launch = true
-    tags = {
-        "Name" = "${var.default_tags.env}-Public-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
-    }
-    availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  ipv6_cidr_block         = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.default_tags.env}-Public-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
 }
 #Private Subnet: 10.0.0.0/24
 resource "aws_subnet" "private" {
-    count = 2
-    vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
-    map_public_ip_on_launch = true
-    tags = {
-        "Name" = "${var.default_tags.env}-Private-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
-    }
-    availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+  count                   = var.private_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.default_tags.env}-Private-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
 }
 
 #Public RT
