014704fabbeecbc50828780831faddbebc62c33a
Jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-16 12:10:59 -0500

    Thank you for your commit

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_vpc" "main" {
 }
 #Public Subnet: 10.0.0.0/24 ensuring no subnets have the same address
 resource "aws_subnet" "public" {
-    count = 2
+    count = var.public_subnet_count
     vpc_id = aws_vpc.main.id
     cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
     ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
@@ -36,8 +36,19 @@ resource "aws_subnet" "public" {
     tags = {
         "Name" = "$(var.default_tags.env)-Public-Subnet"
     }
+    availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
 }
 #Private Subnet: 10.0.0.0/24
+resource "aws_subnet" "private" {
+    count = 2
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
+    map_public_ip_on_launch = true
+    tags = {
+        "Name" = "$(var.default_tags.env)-Private-Subnet"
+    }
+    availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+}
 
 #Public RT
 
