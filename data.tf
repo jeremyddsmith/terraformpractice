b72ec0093fa56e112e9b78edb82c262545cb73a1
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-17 11:22:50 -0500

    Thank you for your commit

--- data.tf
+++ data.tf
@@ -1,8 +1,11 @@
 data "aws_availability_zones" "availability_zone" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
   state = "available"
 }
 
 data "aws_ssm_parameter" "instance_ami" {
-  name = "/aws/service/canonical/ubuntu/eks/20.04/1.22/stable/20220713/amd64/hvm/ebs-gp2/ami-id	
-"
+  name = "/aws/service/canonical/ubuntu/eks/20.04/1.22/stable/20220713/amd64/hvm/ebs-gp2/ami-id"
 }
