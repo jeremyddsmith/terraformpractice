4bc09a7bb611434a5e03542fdb6038cf86a5b007
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-17 11:20:40 -0500

    Thank you for your commit.

---
+++ vpc.tf
@@ -0,0 +1,120 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.32.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = var.default_tags
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block                       = var.vpc_cidr
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  tags = {
+    "Name" = "${var.default_tags.env}-VPC"
+
+  }
+}
+#Public Subnet: 10.0.0.0/24 ensuring no subnets have the same address
+resource "aws_subnet" "public" {
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  ipv6_cidr_block         = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.default_tags.env}-Public-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+}
+#Private Subnet: 10.0.0.0/24
+resource "aws_subnet" "private" {
+  count                   = var.private_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.default_tags.env}-Private-Subnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+}
+
+#Public RT
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "${var.default_tags.env}-Public-RT"
+  }
+}
+
+#route for public route table
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main-igw.id
+}
+
+#public route table association
+
+resource "aws_route_table_association" "public" {
+  count          = var.public_subnet_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+#private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "${var.default_tags.env}-Private-RT"
+  }
+}
+
+# #route for private route table
+
+# resource "aws_route" "private" {
+#   route_table_id         = aws_route_table.private.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.main_NAT.id
+# }
+
+# #private route table association
+
+# resource "aws_route_table_association" "private" {
+#   count          = var.private_subnet_count
+#   subnet_id      = element(aws_subnet.private.*.id, count.index)
+#   route_table_id = aws_route_table.private.id
+# }
+
+#igw 
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "${var.default_tags.env}-IGW"
+  }
+}
+
+# #EIP
+# resource "aws_eip" "NAT_EIP" {
+#   domain = "vpc"
+# }
+
+# #NAT
+# resource "aws_nat_gateway" "main_NAT" {
+#   allocation_id = aws_eip.NAT_EIP.id
+#   subnet_id     = aws_subnet.public[0].id
+#   tags = {
+#     "Name" = "${var.default_tags.env}-NAT-GW"
+#   }
+# } 
