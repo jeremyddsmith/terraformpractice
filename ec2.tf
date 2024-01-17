e813ee4b0d9c6e5de74c92e4234832ac0c76655c
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-17 11:45:30 -0500

    Thank you for your commit

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "main" {
-    ami = data.aws_ssm_parameter.instance_ami
+    ami = data.aws_ssm_parameter.instance_ami.value
     instance_type = "t3.micro"
     key_name = "jdansible"
     subnet_id = aws_subnet.public[0].id
-    vpc_security_group_ids = [sg-0e673f30eb234d1ba]
+    vpc_security_group_ids = ["sg-0e673f30eb234d1ba"]
     tags = {
-        "Name" = "${var.default_tag.env}-EC2"
+        "Name" = "${var.default_tags.env}-EC2"
     }
 }
