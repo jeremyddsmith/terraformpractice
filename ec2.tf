f5642e8f466f8caf5d8402bef08e5784533c7489
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-17 13:20:23 -0500

    Thank you for your commit

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
     instance_type = "t3.micro"
     key_name = "jdansible"
     subnet_id = aws_subnet.public[0].id
-    vpc_security_group_ids = ["sg-07ee03d9e41b765c6"]
+    vpc_security_group_ids = ["sg-07ee03d9e41b765c6"] #must use your vpc's sg
     tags = {
         "Name" = "${var.default_tags.env}-EC2"
     }
