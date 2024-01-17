75351e24921782cdf14f83b1a59f89c6f172c3c0
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-17 11:21:21 -0500

    Thank you for your commit

--- data.tf
+++ data.tf
@@ -1,3 +1,8 @@
 data "aws_availability_zones" "availability_zone" {
   state = "available"
 }
+
+data "aws_ssm_parameter" "instance_ami" {
+  name = "/aws/service/canonical/ubuntu/eks/20.04/1.22/stable/20220713/amd64/hvm/ebs-gp2/ami-id	
+"
+}
