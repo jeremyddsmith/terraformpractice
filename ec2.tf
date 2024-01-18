802d0375d3901cf4ca85f51e5831572e1ac76489
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-18 13:12:32 -0500

    yes

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
   instance_type          = "t3.micro"
   key_name               = "jdansible"
   subnet_id              = aws_subnet.public[0].id
-  vpc_security_group_ids = ["sg-0f43d1623f415f65b"] #must use your vpc's sg
+  vpc_security_group_ids = ["aws_vpc.main.default_security_group_id"] #default sg for vpc
   tags = {
     "Name" = "${var.default_tags.env}-EC2"
   }
