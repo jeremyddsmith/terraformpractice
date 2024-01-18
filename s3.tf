969837fd2b85bc0af7115f8322bb5b0f28d99195
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-18 10:33:04 -0500

    thank you

--- s3.tf
+++ s3.tf
@@ -1,9 +1,38 @@
 resource "aws_s3_bucket" "backend" {
-  bucket = "jddbucket" #must be globally unique
+    bucket = "jddbucket" #must be globally unique
 }
 resource "aws_s3_bucket_versioning" "backend" {
-  bucket = aws.aws_s3_bucket.backend.id
-  versioning_configuration {
-    status = "Disabled"
-  }
+    bucket = aws_s3_bucket.backend.id
+    versioning_configuration {
+        status = "Disabled"
+    }
+}
+resource "aws_s3_bucket_public_access_block" "backend" {
+    bucket = aws_s3_bucket.backend.id
+    block_public_acls   = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "backend" {
+    statement {
+        sid = "Public View"
+        principals {
+            type = "AWS"
+            identifiers = ["*"]
+        }
+        actions = [
+            "s3:*"
+        ]
+    
+    resources = [
+        aws_s3_bucket.backend.arn, "${aws_s3_bucket.backend.arn}/*"
+        ]
+    }
+}
+
+resource "aws_s3_bucket_policy" "backend" {
+    bucket = aws_s3_bucket.backend.id
+    policy = data.aws_iam_policy_document.backend.json
 }
