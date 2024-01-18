80e69fa474b2d7f162cb97332f7c60f5e3154377
jeremyddsmith <jeremyddsmith@gmail.com>
2024-01-18 10:23:29 -0500

    thank you

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,14 @@
 resource "aws_instance" "main" {
-    ami = data.aws_ssm_parameter.instance_ami.value
-    instance_type = "t3.micro"
-    key_name = "jdansible"
-    subnet_id = aws_subnet.public[0].id
-    vpc_security_group_ids = ["sg-07ee03d9e41b765c6"] #must use your vpc's sg
-    tags = {
-        "Name" = "${var.default_tags.env}-EC2"
-    }
-    user_data = base64encode(file("user.sh"))
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  instance_type          = "t3.micro"
+  key_name               = "jdansible"
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = ["sg-06b1270861159e30d"] #must use your vpc's sg
+  tags = {
+    "Name" = "${var.default_tags.env}-EC2"
+  }
+  user_data = base64encode(file("user.sh"))
 }
+
+# output "ec2_ssh_command" {
+#     value = "ssh -i vbansible.pem ubuntu@ec2-${replace(aws_instance.main.public_ip, ".", "-")}.compute-1.amazonaws.com"
